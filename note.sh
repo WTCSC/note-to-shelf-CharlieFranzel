30b1770cf99220b35ea8aaefdaedc37ade8fe5c1
Charlie <charliefranzel@gmail.com>
2025-01-14 10:20:58 -0700

    Works but needs documentation and error handling still

--- note.sh
+++ note.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 time=$(date +"%Y-%m-%d %H:%M:%S")
 if [ "$1" = "add" ]; then
-    echo "${time} -"$2"" >> notes.txt
+    echo "${time} - "$2"" >> notes.txt
     echo Note added successfully
+fi
+if [ "$1" = "list" ]; then
+    cat notes.txt
+fi
+if [ "$1" = "search" ]; then
+    grep -- "$2" "notes.txt"
+fi
+if [ "$1" != "add" ] && [ "$1" != "list" ] && [ "$1" != "search" ]; then
+    exit 1
 fi
